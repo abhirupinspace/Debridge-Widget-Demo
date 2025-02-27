b046c2669f578f8ce56d33d06405a715565232ef
abhirupinspace <abkenway2003@gmail.com>
2025-02-27 23:42:07 +0530

    lint

--- src/app/components/DeBridgeWidget.tsx
+++ src/app/components/DeBridgeWidget.tsx
@@ -1,4 +1,8 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useEffect } from "react";
 
 declare global {
